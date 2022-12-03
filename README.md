1cefa5495b35e46d912729c84a1eec8e02d8b930
Noah Beij <36964954+thedutchgunslinger@users.noreply.github.com>
2022-12-03 02:08:40 +0100

    render script aangepast
    videos zijn nu renderen via het npm run render command en hebben een coole spinner 😎

--- README.md
+++ README.md
@@ -19,14 +19,8 @@ npm i
 npm start
 ```
 
-## Render assets.
-in de index.tsx bestanden van de mappen ```lowerThird ``` en ```qrcode ``` kan je variabele vinden om de tekst of url aan te passen. als je dit gedaan hebt ben je klaar om de assets te renderen.
+## customize assets.
+in de index.tsx bestanden van de mappen ```lowerThird ```, ```qrcode ``` of ```subsciberPercent``` kan je variabele vinden om de tekst of url aan te passen. als je dit gedaan hebt ben je klaar om de assets te renderen.
 
-### Render LowerThird
-```console
-npm run lowerThird
-``` 
-### Render QrCode
-```console
-npm run qrCode
-```
+## render assets
+doormiddel van de custom cli hoef je maar een command te runnen, vanuit daar kan je kiezen welke asset je wilt renderen.
--- render.js
+++ render.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 import chalk from 'chalk';
 import inquirer from 'inquirer';
+import {exec} from 'child_process';
+import {createSpinner} from 'nanospinner';
+
 console.log(chalk.bgGreenBright.bold('CodeCafé Video Asset Generator'));
 await chooseAsset();
 async function chooseAsset() {
@@ -8,10 +11,11 @@ async function chooseAsset() {
 		name: 'asset',
 		type: 'list',
 		message: 'kies een asset om te renderen!\n',
-		choices: ['LowerThird', 'QRcode','SubsciberPercentage'],
+		choices: ['LowerThird', 'QRcode', 'SubsciberPercentage'],
 	});
 	return loadAssetOptions(chosenAsset.asset);
 }
+
 async function loadAssetOptions(asset) {
 	if (asset === 'LowerThird') {
 		renderLowerThird();
@@ -21,6 +25,7 @@ async function loadAssetOptions(asset) {
 		renderSubsciberPercentage();
 	}
 }
+
 async function renderLowerThird() {
 	const text = await inquirer.prompt({
 		name: 'text',
@@ -30,8 +35,20 @@ async function renderLowerThird() {
 	if (text.text == null || text.text == '') {
 		text.text = 'Test Title';
 	}
-	console.log(text.text);
+	console.log(text.text + ' || helaas nog niet in de video kunnen zetten :(');
+	const spinner = createSpinner('rendering video asset').start();
+
+	var os = new os_func();
+
+	os.execCommand('npm run lowerThird', function () {
+		spinner.success({
+			text:
+				'video is gerendered en hier te vinden: ' +
+				chalk.greenBright('./out/Lower-Third.mp4'),
+		});
+	});
 }
+
 async function renderQrCode() {
 	const text = await inquirer.prompt({
 		name: 'url',
@@ -41,8 +58,20 @@ async function renderQrCode() {
 	if (text.url == null || text.url == '') {
 		text.url = 'https://discord.com/invite/xwv8ptXUdh';
 	}
-	console.log(text.url);
+	console.log(text.url + ' || helaas nog niet in de video kunnen zetten :(');
+	const spinner = createSpinner('rendering video asset').start();
+
+	var os = new os_func();
+
+	os.execCommand('npm run qrCode', function () {
+		spinner.success({
+			text:
+				'video is gerendered en hier te vinden: ' +
+				chalk.greenBright('./out/QR-Code.mp4'),
+		});
+	});
 }
+
 async function renderSubsciberPercentage() {
 	const text = await inquirer.prompt({
 		name: 'percent',
@@ -52,6 +81,31 @@ async function renderSubsciberPercentage() {
 	if (text.percent == null || text.percent == '') {
 		text.percent = 75;
 	}
-	console.log(text.percent);
+	console.log(
+		text.percent + ' || helaas nog niet in de video kunnen zetten :('
+	);
+	const spinner = createSpinner('rendering video asset').start();
+
+	var os = new os_func();
+
+	os.execCommand('npm run subPerc', function () {
+		spinner.success({
+			text:
+				'video is gerendered en hier te vinden: ' +
+				chalk.greenBright('./out/Subscriber-Percent.mp4'),
+		});
+	});
 }
 
+function os_func() {
+	this.execCommand = function (cmd, callback) {
+		exec(cmd, (error, stdout, stderr) => {
+			if (error) {
+				console.error(`exec error: ${error}`);
+				return;
+			}
+
+			callback(stdout);
+		});
+	};
+}
